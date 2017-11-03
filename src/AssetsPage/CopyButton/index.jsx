1786883abed7a77b6aed6fd6fed0de98b2a0de5e
Michael Roytman <mroytman@edx.org>
2017-11-03 13:11:51 -0400

    add CopyButton component and a mock for the copy-to-clipboard package

---
+++ src/AssetsPage/CopyButton/index.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from '@edx/paragon/src/Button';
+import classNames from 'classnames';
+import copy from 'copy-to-clipboard';
+
+export default class CopyButton extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      wasClicked: false,
+    };
+  }
+
+  render() {
+    return (
+      <Button
+        label={!this.state.wasClicked ? this.props.label : this.props.onCopyLabel }
+        onClick={() => {
+          this.setState({
+            wasClicked: true,
+          });
+          copy(`${this.props.textToCopy}`);
+        }}
+        onBlur={() => {
+          this.setState({
+            wasClicked: false,
+          });
+        }}
+      />
+    );
+  }
+}
+
+CopyButton.propTypes = {
+  label: PropTypes.oneOfType([PropTypes.string, PropTypes.element]).isRequired,
+  onCopyLabel: PropTypes.oneOfType([PropTypes.string, PropTypes.element]).isRequired,
+  textToCopy: PropTypes.string.isRequired,
+};
